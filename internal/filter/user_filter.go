60dc824ec11d79686cd4da7149f39e6d830d97cd
Duc Nguyen <minhduc140583@gmail.com>
2023-03-19 13:52:59 +0700

    Add yaml

--- internal/filter/user_filter.go
+++ internal/filter/user_filter.go
@@ -3,12 +3,12 @@ package filter
 import . "go-service/internal/model"
 
 type UserFilter struct {
-	Id        string `mapstructure:"id" json:"id" gorm:"column:id;primary_key" bson:"_id" dynamodbav:"id" firestore:"id" match:"equal" validate:"required,max=40"`
-	Username  string `mapstructure:"username" json:"username" gorm:"column:username" bson:"username" dynamodbav:"username" firestore:"username" match:"prefix" validate:"required,username,max=100"`
-	Email     string `mapstructure:"email" json:"email" gorm:"column:email" bson:"email" dynamodbav:"email" firestore:"email" match:"prefix" validate:"email,max=100"`
-	Phone     string `mapstructure:"phone" json:"phone" gorm:"column:phone" bson:"phone" dynamodbav:"phone" firestore:"phone" validate:"required,phone,max=18"`
-	PageIndex int64  `mapstructure:"pageIndex" json:"pageIndex,omitempty" gorm:"column:pageIndex" bson:"pageIndex,omitempty" dynamodbav:"pageIndex,omitempty" firestore:"pageIndex,omitempty"`
-	PageSize  int64  `mapstructure:"pageSize" json:"pageSize,omitempty" gorm:"column:pageSize" bson:"pageSize,omitempty" dynamodbav:"pageSize,omitempty" firestore:"pageSize,omitempty"`
+	Id        string `yaml:"id" mapstructure:"id" json:"id" gorm:"column:id;primary_key" bson:"_id" dynamodbav:"id" firestore:"id" match:"equal" validate:"required,max=40"`
+	Username  string `yaml:"username" mapstructure:"username" json:"username" gorm:"column:username" bson:"username" dynamodbav:"username" firestore:"username" match:"prefix" validate:"required,username,max=100"`
+	Email     string `yaml:"email" mapstructure:"email" json:"email" gorm:"column:email" bson:"email" dynamodbav:"email" firestore:"email" match:"prefix" validate:"email,max=100"`
+	Phone     string `yaml:"phone" mapstructure:"phone" json:"phone" gorm:"column:phone" bson:"phone" dynamodbav:"phone" firestore:"phone" validate:"required,phone,max=18"`
+	PageIndex int64  `yaml:"page_index" mapstructure:"page_index" json:"pageIndex,omitempty" gorm:"column:page_index" bson:"pageIndex,omitempty" dynamodbav:"pageIndex,omitempty" firestore:"pageIndex,omitempty"`
+	PageSize  int64  `yaml:"page_size" mapstructure:"page_size" json:"pageSize,omitempty" gorm:"column:page_size" bson:"pageSize,omitempty" dynamodbav:"pageSize,omitempty" firestore:"pageSize,omitempty"`
 }
 
 type Result struct {
